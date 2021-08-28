703a5285cf6fd7388aaaae2cdb13f11c1694f6ed
sakamuuy <sakamuu.y@gmail.com>
2021-08-28 13:40:07 +0900

    Add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital@1&display=swap');
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+      Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
